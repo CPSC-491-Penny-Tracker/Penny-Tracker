49a8754307a9f955717ddca18d23f7ffacf81e8a
Makhafagy <makhafagy11@gmail.com>
2022-09-22 20:23:23 -0700

    codebase initialized

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+# Users Table
+    # first name
+    # last name
+    # email
+    # password
+
+# Planner Table
+#   site
+#   budget
+#   total
+#   cart total
+#   time period
+
+# Summary Table
+#   amazon summary
+#   ebay summary
+#   bestbuy summary
+#   walmart summary
+#   total summary
