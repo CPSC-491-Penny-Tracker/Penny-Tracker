1bf98dd5d27d1395ebcb639b86aa976664bb935a
Mahmoud Khafagy <mahmoudk@csu.fullerton.edu>
2022-12-01 16:50:37 -0800

    update to the components, improved tabs, login, log out customizations

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -6,6 +6,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import UserContext from "../context/UserContext";
 import JwtService from "../service/JwtService";
+import "./tab.css";
 
 const Tab = () => {
   const context = useContext(UserContext)
@@ -16,24 +17,43 @@ const Tab = () => {
     navigate('/users')
   }
 
-  const loggedInNav = () => (
-    <React.Fragment>
-      <Nav.Link href="#/planner" className="align-middle">Planner</Nav.Link>
-      <Nav.Link href="#/summary" className="align-middle">Summary</Nav.Link>
-    </React.Fragment>
-  )
+  const loggedInNav = () => {
+    return (
+      <React.Fragment>
+        <div className="tabs">
+          <Nav.Link href="#/planner" className="tab">Planner</Nav.Link>
+          <Nav.Link href="#/summary" className="tab">Summary</Nav.Link>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-  const loggedIn = () => 
-    <Button variant="outline-danger" onClick={logout}>Logout</Button>
+  const loggedIn = () => {
+    return (
+      <React.Fragment>
+        <div className="log3">
+          <Button variant="outline-danger" onClick={logout}>Logout</Button>
+        </div>
+      </React.Fragment>
+    );
+  }
   
-  const loggedOut = () => (
-    <React.Fragment>
-      <Nav.Link href="#/register" className="d-flex align-items-center me-3">Register</Nav.Link>
-      <NavLink to="/users">
-        <Button variant="outline-success">Login</Button>
-      </NavLink>
-    </React.Fragment>
-  )
+  const loggedOut = () => {
+    return(
+      <React.Fragment>
+        <div className="log">
+          <Nav.Link href="#/register">
+            <Button variant="outline-success">Sign up</Button>
+          </Nav.Link>
+        </div>
+        <div className="log2">
+          <NavLink to="/users">
+            <Button variant="outline-success">Login</Button>
+          </NavLink>
+        </div>
+      </React.Fragment>
+    );
+  }
 
   return (
     <Navbar bg="light" expand="lg">
@@ -41,14 +61,18 @@ const Tab = () => {
         <Navbar.Brand disabled>{JwtService.hasAuthToken() ? `Hello, ${context.user.username}` : 'Penny Tracker'} </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll"/>
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#/product" className="align-middle">Compare</Nav.Link>
-            {JwtService.hasAuthToken() && loggedInNav()}
-          </Nav>
+          <div className="tab-tab">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <div className="tabs">
+                <Nav.Link href="#/product" className="tab">Compare</Nav.Link>
+                {JwtService.hasAuthToken() && loggedInNav()}
+              </div>
+            </Nav>
+          </div>
           <div className="d-flex">
             {JwtService.hasAuthToken() ? loggedIn() : loggedOut()}
           </div>
